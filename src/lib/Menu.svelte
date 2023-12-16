761faaa27bc6f538a9056380158cd06fcf77fe72
miguelgargallo <it@itamaesan.org>
2023-12-16 15:24:43 +0100

    Added menu

---
+++ src/lib/Menu.svelte
@@ -0,0 +1,10 @@
+<!-- Menu.svelte -->
+
+<nav class="flex flex-row justify-between items-center p-4 bg-orange-200">
+	<ul class="flex flex-row space-x-4 text-lg">
+		<li><a href="https://npmjs.com/package/toas" target="_blank" rel="noreferrer">NPM</a></li>
+		<li>
+			<a href="https://github.com/itamaesanorg/toas" target="_blank" rel="noreferrer">GitHub</a>
+		</li>
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
+	import Menu from '../lib/Menu.svelte';
 	import '../app.css';
 </script>
 
-<slot />
+<!-- all inside a div -->
+<div class="">
+	<div class="flex flex-row items-center p-4 bg-orange-200">
+		<Menu />
+	</div>
+
+	<main class="flex flex-col items-center justify-center p-4 bg-black">
+		<slot />
+	</main>
+</div>
